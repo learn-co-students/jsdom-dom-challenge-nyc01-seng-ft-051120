16c4e3703719cbf1f22a1546ab79de319bd53490
kevinneyer <kevinneyer@gmail.com>
2020-06-24 15:52:49 -0400

    Second Commit

--- js/challenge.js
+++ js/challenge.js
@@ -7,7 +7,7 @@ let clockTimer = window.setInterval(countUp, 1000)
 
 document.addEventListener('click', function(e){
     console.log(e.target)
-
+    
     if (e.target.id === "minus"){
         let myCounter = parseInt(counter.innerText)
         counter.innerText = myCounter - 1 }
@@ -18,11 +18,21 @@ document.addEventListener('click', function(e){
         let likeContainer = document.getElementsByTagName('ul')
         let like = document.createElement('li')
         like.innerHTML = `${counter.innerText} has been liked `
-        likeContainer.append(like)
+        likeContainer.append(like)}
+    })
     
+    
+ document.addEventListener('submit', function(e){
+    e.preventDefault();
+        // if (e.target.id === "Submit"){
+        let list = document.getElementById('list')
+        let p = document.createElement('p')
+        let input = document.getElementById('comment-input')
+        p.innerHTML = input.value 
+        list.append(p)
+        e.target.reset()
+    })
 
-    }
-})
 
    
 
