04a08ca0fb2f31d2a390a0d61893b26cdeb5eb47
ChayaGreisman <chayamgreisman@gmail.com>
2020-06-24 15:10:43 -0700

    finished

--- js/challenge.js
+++ js/challenge.js
@@ -1,98 +1,103 @@
-let count = 0
+
 
 document.addEventListener("DOMContentLoaded", () => {
+    let count = 0
+    const likeCount = {}
 
     let htmlCounter = document.getElementById("counter")
-
     let ourInterval = setInterval(function(){count++; htmlCounter.innerText = count;},1000)
+    
+    document.addEventListener("submit", function(e){
+        e.preventDefault()
+        const form = e.target
+        const comment = form.comment.value
+        const p = document.createElement('p')
+        p.textContent = comment
+        document.querySelector('#list').append(p)
+        form.reset()
+    })
 
-    let minusButton = document.getElementById("minus")
-    let plusButton = document.getElementById("plus")
+    // function handleClicks(){
 
+    
 
-    function handleClicks(){
+    document.addEventListener("click", function(e){
 
-        const body = document.getElementsByTagName("body")[0]
+        
+        if (e.target.id==="minus"){
+            count--
+            htmlCounter.innerText = count
+        }
 
-        body.addEventListener("click", function(e){
+        if (e.target.id==="plus"){
+            count++
+            htmlCounter.innerText = count
+        }
 
-            
-            if (e.target.id==="minus"){
-                count--
-                htmlCounter.innerText = count
-            }
+        if (e.target.id==="pause"){
 
-            if (e.target.id==="plus"){
-                count++
-                htmlCounter.innerText = count
-            }
+            let pauseButton = e.target
 
-            if (e.target.id==="pause"){
-
-                let pauseButton = e.target
-
-                if (pauseButton.innerText === 'pause'){
-                    
-                    clearInterval(ourInterval)
-                    let minusButton = document.getElementById("minus")
-                    minusButton.disabled = true
-                    let plusButton = document.getElementById("plus")
-                    plusButton.disabled = true
-                    let heartButton = document.getElementById("heart")
-                    heartButton.disabled = true
-                    pauseButton.innerText = 'resume'
-                    
-                }
-
-                else if (pauseButton.innerText === 'resume'){
-
-                    let ourInterval = setInterval(function(){count++; htmlCounter.innerText = count;},1000)
-                    let minusButton = document.getElementById("minus")
-                    minusButton.disabled = false
-                    let plusButton = document.getElementById("plus")
-                    plusButton.disabled = false
-                    let heartButton = document.getElementById("heart")
-                    heartButton.disabled = false
-                    pauseButton.innerText = 'pause'
-                }
-
-
-            }
-
-            if (e.target.id=="heart"){
-
-                const liAll = document.getElementsByTagName('li')
-                const liArray = Array.from(liAll)
-
-                let existingLiLike = document.querySelector(`li[data-num=${count}]`)
+            if (pauseButton.innerText === 'pause'){
                 
-                if (liArray.includes(existingLiLike)) {
-                    
-                    let likeCount = existingLiLike.querySelector("span").innerText
-                    existingLiLike.innerHTML = `${count} has been liked <span>${likeCount.parseInt++}</span> times`
-
-                } else {
-
-                    let  likeCount=1
-                    let newLikeLi = document.createElement('li')
-                    // newLikeLi.dataset.num = count
-                    newLikeLi.innerHTML = `${count} has been liked <span>${likeCount}</span> time`
-                    
-                    let ul = document.getElementsByClassName("likes")[0]
-                    ul.appendChild(newLikeLi)
-            
-                }
-
+                clearInterval(ourInterval)
+                let minusButton = document.getElementById("minus")
+                minusButton.disabled = true
+                let plusButton = document.getElementById("plus")
+                plusButton.disabled = true
+                let heartButton = document.getElementById("heart")
+                heartButton.disabled = true
+                let submitButton = document.getElementById("submit")
+                submitButton.disabled = true
+                pauseButton.innerText = 'resume'
+                
             }
 
-        })
+            else if (pauseButton.innerText === 'resume'){
+
+                ourInterval = setInterval(function(){count++; htmlCounter.innerText = count;},1000)
+                let minusButton = document.getElementById("minus")
+                minusButton.disabled = false
+                let plusButton = document.getElementById("plus")
+                plusButton.disabled = false
+                let heartButton = document.getElementById("heart")
+                heartButton.disabled = false
+                let submitButton = document.getElementById("submit")
+                submitButton.disabled = false
+                pauseButton.innerText = 'pause'
+            }
+
+
+        }
+
+
+        if (e.target.id=="heart"){
+
+        const counter = document.getElementById('counter')
+        const currentNum = parseInt(counter.textContent)
+        
+            if(!likeCount[currentNum]){ // number is not in likeCount
+            likeCount[currentNum] = 1
+            const ul = document.querySelector('.likes')
+            const li = document.createElement('li')
+            li.dataset.number = currentNum
+            li.textContent = `${currentNum} has been liked 1 time`
+            ul.append(li)
+            } else if(likeCount[currentNum]) { // number has been liked
+            likeCount[currentNum] += 1
+            const li = document.querySelector(`[data-number="${currentNum}"]`)
+            li.textContent = `${currentNum} has been liked ${likeCount[currentNum]} times`
+            }
+        }
+
+    })
 
 
 
-    }
+    // }
     
     
-    handleClicks()
+    // handleClicks()
     
     
 
